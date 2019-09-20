6374211590e15bd5ebc5eecdc44c9032d37ff5e2
Nicole C <nicas@sonic.net>
2019-09-20 15:17:22 -0700

    InfixToPostFix mostly finalized, debugging still needed

--- src/InfixToPostfixConverter.java
+++ src/InfixToPostfixConverter.java
@@ -39,35 +39,94 @@ public class InfixToPostfixConverter implements ExpressionConverterInterface
     
     public String convert(String expression)
     {
-        try {
-            StackInterface<Character> stack = new LinkedStack<>();
-            String outputExpression = null;
+        StackInterface<Character> stack = new LinkedStack<>();
+        String outputExpression = null;
 
-            for (int i = 0; i < expression.length(); i++)
-            {
-                // if the term is an operand append it to the end of the output expression
-                if (Character.isLetter(expression.charAt(i)))
-                    outputExpression += expression.charAt(i);
+        for (int i = 0; i < expression.length(); i++)
+        {
+            // if the term is an operand append it to the end of the output expression
+            if (Character.isLetter(expression.charAt(i)))
+                outputExpression += expression.charAt(i);
 
-                // else if the term is the ^ operator push it onto the stack
-                else if (expression.charAt(i) == '^')
-                    stack.push('^');
+            // else if the term is the ^ operator push it onto the stack
+            else if (expression.charAt(i) == '^')
+                stack.push(expression.charAt(i));
 
-                // else if the term is and operator +, -, *, or /
+            // else if the term is and operator +, -, *, or
                 // pop operators from the stack appending them to the output expression
                 // until the stack is empty or the operator on the top of the stack has
                 // a lower precedence than the new operator. Then push the new operator
                 // onto the stack.
-                else if (expression.charAt(i) == '+' || expression.charAt(i) == '-' || expression.charAt(i) == '*' || expression.charAt(i) == '/') {
-                    
-                }
-                    
+            else if (expression.charAt(i) == '+' || expression.charAt(i) == '-' || expression.charAt(i) == '*' || expression.charAt(i) == '/')
+            {
 
+                int precedenceOfExpression = checkPrecedence(expression.charAt(i));
+
+                while (!stack.isEmpty())
+                {
+                    if (precedenceOfExpression <= checkPrecedence(stack.peek()) && (stack.peek() != '('))
+                        outputExpression += stack.pop();
+                    else
+                        break;
+                }
+
+                stack.push(expression.charAt(i));
             }
-        } catch (InvalidExpressionException thing) {
-            System.out.println("InvalidExpressionException catch successful");
+
+            // else if the term is a ( push it onto the stack
+            else if (expression.charAt(i) == '(')
+                stack.push(expression.charAt(i));
+
+            // else if the term is a ) pop operators from the stack and append them to the output expression
+            // until the ( delimiter is found. discard both ( and ), they should not be
+            // appended to the output expression.
+            else if (expression.charAt(i) == ')')
+            {
+                while (stack.peek() != '(')
+                    outputExpression += stack.pop();
+
+                stack.pop();
+            }
+
+            // else if the term is white space continue
+            else if (expression.charAt(i) == ' ')
+                continue;
+
+            // else if the term is unknown throw an InvalidExpressionException
+            else {
+                throw new InvalidExpressionException();
+            }
+
         }
 
-        return null;
+        while (!stack.isEmpty())
+        {
+            // pop an operator from the stack
+            // if it is a ( delimiter throw an InvalidExpressionException
+            // else append the operator to the output expression
+            if (stack.peek() != '(')
+                outputExpression += stack.pop();
+            else
+                throw new InvalidExpressionException();
+        }
+
+        return outputExpression;
+    }
+
+    private int checkPrecedence(char operator) {
+        switch (operator)
+        {
+            case '^':
+                return 17;
+            case '+':
+            case '-':
+                return 14;
+            case ')':
+            case '(':
+                return 13;
+            case '*':
+            case '/':
+                return 12;
+        }
     }
 }
